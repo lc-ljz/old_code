f7fb526ddf032884766db8828f7b7daba9517356
lc-ljz <1652156296@qq.com>
2022-07-31 11:06:32 +0800

    second commit

---
+++ VisualStudiocode/oldcode/ConsoleApplication2/ConsoleApplication2/ControlStyle.h
@@ -0,0 +1,49 @@
+#pragma once
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 resource.rc 使用
+//
+#define IDI_ICONBIG                     101
+#define IDI_ICONSMALL                   105
+#define IDI_ABOUT                       106
+#define IDR_WAVE1                       107
+#define IDR_CONGRATULATIONS             109
+#define IDS_STRING_APPTITLE             110
+#define IDS_STRING_HIGHSCORE            111
+#define IDS_STRING_SCORE                112
+#define IDS_STRING_NEWGAME              113
+#define IDS_STRING_RAMDOM               114
+#define IDS_STRING_RANDOM               114
+#define IDS_STRING_REDO                 115
+#define IDS_STRING_SCOREBOARD           116
+#define IDS_STRING_ABOUT                117
+#define IDS_STRING_MEMO1                118
+#define IDS_STRING_MEMO2                119
+#define IDS_STRING_CLOSETEXT            120
+#define IDS_STRING_CLOSE                121
+#define IDS_STRING_FAILTOSAVE           122
+#define IDS_STRING_PROMPT               123
+#define IDS_STRING_STARTRANDOMTEXT      126
+#define IDS_STRING_STARTRANDOM          127
+#define IDS_STRING_SCOREBOARD1          128
+#define IDS_STRING_SCOREBOARD2          129
+#define IDS_STRING_SCOREBOARD3          130
+#define IDS_STRING_ABOUTTEXT1           131
+#define IDS_STRING_ABOUTTEXT2           132
+#define IDS_STRING_ABOUTTEXT3           133
+#define IDS_STRING_OK                   134
+#define IDC_EDITNAME                    1002
+#define IDC_STATIC2                     1005
+#define IDC_STATIC1                     1006
+#define IDC_STATIC3                     1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
